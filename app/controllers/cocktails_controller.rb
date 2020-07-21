d86d0c7d9ce9010af91f2c1e86dced3e7be0e781
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2020-07-21 21:31:12 +0100

    checked seed working and initial cocktail view set-up

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,39 @@
+class CocktailsController < ApplicationController
+    before_action :find_cocktail, only: [:show, :edit, :update]
+    def index 
+      @cocktails = Cocktail.all
+    end
+
+    def show
+      @dose = Dose.new
+    end
+
+    def new
+      @cocktail = Cocktail.new
+    end
+
+    def create
+      @cocktail = Cocktail.new(cocktail_params)
+      @cocktail.save
+      redirect_to root_path
+    end
+    
+    def edit
+    end
+
+    def update
+      @cocktail.update(cocktail_params)
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    end
+    
+    private
+
+    def find_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    def cocktail_params
+        params.require(:cocktail).permit(:name)
+    end
+end
