8ee35e20b65b04ffd923903d018c1bbeff6454b0
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2020-07-21 20:06:31 +0100

    seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+api_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+puts 'Creating ingredients'
+
+open(api_url) do |list|
+    ingredients = JSON.parse(list.read)
+    ingredients['drinks'].each do |ingredient|
+        Ingredient.create!(
+            name: ingredient['strIngredient1']
+        )
+    end
+end
+
+puts 'Ingredients created'
